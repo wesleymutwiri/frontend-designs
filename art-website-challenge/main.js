75280096e412cfedef7b403406620906c07ead9e
wesleymutwiri <wesleymochiemo@gmail.com>
2021-02-04 14:57:39 +0300

    users section finished already

---
+++ art-website-challenge/main.js
@@ -0,0 +1,9 @@
+window.onscroll = function() { 
+    scrollFunction(); 
+}
+
+let nav = document.getElementById("navbar");
+
+function scrollFunction() {
+    nav.classList.toggle("on-scroll", nav.scrollTop > 50 || document.documentElement.scrollTop > 50);
+}
