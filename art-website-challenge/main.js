092d23f7e100cf047667bab715bf2cd644c488d5
Wesley Mutwiri <wesleymochiemo@gmail.com>
2021-06-22 14:17:58 +0300

    Delete main.js

--- art-website-challenge/main.js
+++
@@ -1,9 +0,0 @@
-// window.onscroll = function() { 
-//     scrollFunction(); 
-// }
-
-// let nav = document.getElementById("navbar");
-
-// function scrollFunction() {
-//     nav.classList.toggle("on-scroll", nav.scrollTop > 50 || document.documentElement.scrollTop > 50);
-// }
